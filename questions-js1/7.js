fd787c5721b2bd3c3a3abb3fdf12e073eb9ba240
Hussein Bahdon <35459369+Dead-Section@users.noreply.github.com>
2023-02-18 11:11:10 +0000

    Hussein & Onur - london10

--- questions-js1/7.js
+++ questions-js1/7.js
@@ -3,3 +3,11 @@
 
 const currentYear = 2023;
 currentYear();
+
+// Uncaught TypeError: currentYear is not a function
+    
+
+// nothing will happen because you are calling it as a medthod isnt of a value so you 
+// would do 
+
+console.log(currentYear)
