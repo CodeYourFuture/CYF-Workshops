ceb626a877a0ed231e772ec605ed075aca5622a6
CatSmith1 <105733850+CatSmith1@users.noreply.github.com>
2023-02-25 13:08:10 +0000

    Update 6.js

--- js1-week-2/6.js
+++ js1-week-2/6.js
@@ -3,10 +3,10 @@
 
 
 function printTimesTables(n) {
-    const count = 1;
+    let count = 1;
     while(count <= 12) {
-        const product = n * i;
-        console.log(`${n} * ${i} = ${product}`);
+        const product = n * count;
+        console.log(`${n} * ${count} = ${product}`);
         count++;
     }
 }
