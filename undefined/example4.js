0564b34ef478a28a8734476630d8c59fe8515ba4
Khlil1313 <90422263+Khlil1313@users.noreply.github.com>
2023-03-04 13:13:50 +0000

    update undefined Q4

--- undefined/example4.js
+++ undefined/example4.js
@@ -1,4 +1,5 @@
 let letters = ['a', 'b', 'c'];
-console.log(arr[3]);
+console.log(letters[3]);
 
 // there is no element with the index 3, there is only 0, 1 and 2
+// there is no arryay called arr, instead we have to change to letters
