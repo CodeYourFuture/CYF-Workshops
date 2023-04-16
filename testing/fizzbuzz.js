3a38267ae14236327020937f5934fa8152711251
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-04-16 14:27:57 +0100

    fizzBuzz

--- testing/fizzbuzz.js
+++ testing/fizzbuzz.js
@@ -1,3 +1,12 @@
-function fizzbuzz(number) {}
+function fizzbuzz(number) {
+    
+    if(number % 3 === 0){
+        return 'Fizz'
+    }else if(number % 5 === 0){
+        return 'Buzz'
+    }else if(number === 1) {
+        return "1"
+    }
+}
 
 export default fizzbuzz;
--- testing/fizzbuzz.test.js
+++ testing/fizzbuzz.test.js
@@ -8,3 +8,13 @@ test("it should return 1 when passed 1 as an argument", () => {
   // assert
   expect(result).toEqual("1");
 });
+test("it should return Fizz when number % 3 === 0", () => {
+  let number  = 6
+  let result = fizzbuzz(number);
+  expect(result).toEqual('Fizz')
+})
+test("it should return Buzz when number % 5 === 0", () => {
+  let number = 10
+  let result = fizzbuzz(number);
+  expect(result).toEqual('Buzz')
+})
