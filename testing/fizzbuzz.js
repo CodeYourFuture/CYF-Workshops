e355701a27d99443c392de2f69b8744ac94b6e0f
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2023-04-18 19:46:55 +0100

    completed

--- testing/fizzbuzz.js
+++ testing/fizzbuzz.js
@@ -1,3 +1,32 @@
-function fizzbuzz(number) {}
+// function fizzbuzz(number) {
+//   let numbers = [];
+
+//   for (let i = 1; i <= number; i++) {
+//     if (i % 3 == 0) {
+//       let i = "fizz";
+//     }
+//     numbers.push(i);
+//   }
+//   return numbers.join(", ");
+// }
+
+// export default fizzbuzz;
+
+// function fizzbuzz(number) {}
+function fizzbuzz(number) {
+  const numbers = [];
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      numbers.push("fizzbuzz");
+    } else if (i % 3 == 0) {
+      numbers.push("fizz");
+    } else if (i % 5 == 0) {
+      numbers.push("buzz");
+    } else {
+      numbers.push(i);
+    }
+  }
+  return numbers.join(", ");
+}
 
 export default fizzbuzz;
--- testing/fizzbuzz.test.js
+++ testing/fizzbuzz.test.js
@@ -8,3 +8,41 @@ test("it should return 1 when passed 1 as an argument", () => {
   // assert
   expect(result).toEqual("1");
 });
+
+test("it should return 2 when passed 2 as an argument", () => {
+  // arrange
+  let number = 2;
+  // act
+  let result = fizzbuzz(number);
+  // assert
+  expect(result).toEqual("1, 2");
+});
+
+test("it should return fizz when passed 3 as an argument", () => {
+  // arrange
+  let number = 3;
+  // act
+  let result = fizzbuzz(number);
+  // assert
+  expect(result).toEqual("1, 2, fizz");
+});
+
+test("it should return buzz when passed 5 as an argument", () => {
+  // arrange
+  let number = 5;
+  // act
+  let result = fizzbuzz(number);
+  // assert
+  expect(result).toEqual("1, 2, fizz, 4, buzz");
+});
+
+test("it should return fizzbuzz when passed 15 as an argument", () => {
+  // arrange
+  let number = 15;
+  // act
+  let result = fizzbuzz(number);
+  // assert
+  expect(result).toEqual(
+    "1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz"
+  );
+});
