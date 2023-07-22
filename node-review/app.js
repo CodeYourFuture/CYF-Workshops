6b318076e5c97d31d8f0b79094de655a231a0fdf
HereshT <hiresh.turky@gmail.com>
2023-07-22 15:32:08 +0100

    workshop-500

--- node-review/app.js
+++ node-review/app.js
@@ -1,39 +1,37 @@
-const express = require('express');
+const express = require("express");
 // loading the express module on line one
-
-
 
 const server = express();
 // initialise a server
 
+const albums = [
+  {
+    title: "Afrique Victime",
+    artist: "Mdou Moctar",
+    yearOfRelease: 2021,
+  },
+  {
+    title: "Africa Brasil",
+    artist: "Jorge Ben Jor",
+    yearOfRelease: 1976,
+  },
+  {
+    title: "Doing it in Lagos,L Boogie,Pop & Disco in 1980's Nigeria",
+    artist: "Compilation",
+    yearOfRelease: 2016,
+  },
+];
 
-
-
-server.get("/",function() {
-    response.status(200).send("You've successfully reached the server");
+server.get("/", function () {
+  response.status(200).send("You've successfully reached the server");
 });
 // set up a "/" endpoint
 // it should respond with "You've successfully reached the server"
 // it should respond with status code 200
+server.get("/albums", function (request, response) {
+  response.status(200).json(albums);
+});
 
-
-const albums = [{
-    title : "Afrique Victime",
-    artist: "Mdou Moctar",
-    yearOfRelease: 2021
-    
-},
-{
-    title: 'Africa Brasil',
-    artist: 'Jorge Ben Jor',
-    yearOfRelease: 1976
-},
-{
-    title: "Doing it in Lagos,L Boogie,Pop & Disco in 1980's Nigeria",
-    artist: 'Compilation',
-    yearOfRelease: 2016
-}];
-
-server.listen(9090,() => {
-    console.log("the server is listening on port 9090...");
-})
+server.listen(9090, () => {
+  console.log("the server is listening on port 9090...");
+});
