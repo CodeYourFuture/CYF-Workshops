a25fdf7682d273a466875d7aca8c80fec3564012
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-10-20 19:04:14 +0100

    add level-1000

---
+++ arrays/level-1000.js
@@ -0,0 +1,6 @@
+// Ensure you're in the arrays directory of this repo
+// Log the return value of fs.readdirSync below, then store it in an appropriately named variable
+// Now iterate and print out only the js files in the array
+const fs = require("fs");
+
+fs.readdirSync("./");
