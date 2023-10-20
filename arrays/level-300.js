f8daca60a57ca5cd3ccdedd6ee4c20c1920cfd62
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-10-20 17:31:23 +0100

    improve arrays questions

--- arrays/level-300.js
+++ arrays/level-300.js
@@ -1,18 +1,47 @@
+// Predict and explain...
+
 // the head of an array is the first element of an array
 // the tail of an array is the last element of an array
 
+// predict and explain what will get printed to the terminal
 
-const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
 function getHead(arr) {
-    return arr.shift();   
-};
+  return arr.shift();
+}
 
 function getTail(arr) {
-    return arr.pop();
-};
+  return arr.pop();
+}
 
 console.log(`The first letter of the alphabet is ${getHead(alphabet)}`);
 console.log(`The last letter of the alphabet is ${getTail(alphabet)}`);
 console.log(alphabet.length);
-// explain why alphabet length is now 24
---
+++ arrays/level-400.js
@@ -0,0 +1,14 @@
+// explain why alphabet length is now 24
+
+const daysOfTheWeek = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const weekend = daysOfTheWeek.splice(5, 2);
+const weekdays = daysOfTheWeek;
