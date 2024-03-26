476660b316e99ca77301ddfab087071b6e808fdd
girdenis-p <girdenis.p@gmail.com>
2024-03-26 17:03:39 +0000

    update test suite according to code review

--- dom-merge-conflict/tasks/conditional-rendering/test/profilePreview.test.js
+++ dom-merge-conflict/tasks/conditional-rendering/test/profilePreview.test.js
@@ -9,54 +9,35 @@ import { ProfilePreview } from "../src/profilePreview";
 
 describe("profile preview", () => {
   describe("default options", () => {
-    const profile = {
-      pictureSrc: "/test",
-      name: "John Doe",
-      bio: "Lorem ipsum dolor sit amet.",
-    };
-    //Freeze the object so that we can reference the properties in test and assume they are the same as above.
-    Object.freeze(profile);
+    test("contains profile info", () => {
+      const profile = {
+        pictureSrc: "/test",
+        name: "John Doe",
+        bio: "Lorem ipsum dolor sit amet.",
+      };
+      //Freeze the object so that we can reference the properties in test and assume they are the same as above.
+      Object.freeze(profile);
 
-    const container = ProfilePreview(profile);
+      const container = ProfilePreview(profile);
 
-    test("contains provided image", () => {
       const image = getByTestId(container, "profilePicture");
-
-      expect(image.src).toContain(profile.pictureSrc);
-    });
-
-    test("contains provided name", () => {
       const name = getByTestId(container, "profileName");
-
-      expect(name).toHaveTextContent(profile.name);
-    });
-
-    test("contains provided bio", () => {
       const bio = getByTestId(container, "profileBio");
 
+      expect(image.src).toContain(profile.pictureSrc);
+      expect(name).toHaveTextContent(profile.name);
       expect(bio).toHaveTextContent(profile.bio);
-    });
-
-    test("contains 3 elements", () => {
       expect(container.childElementCount).toBe(3);
     });
   });
 
   describe.skip("available option", () => {
-    let profile = {};
-
-    beforeEach(() => {
-      profile = {
+    test("only notice that preview is unavailable is displayed with shortform on, available off", () => {
+      const profile = {
         pictureSrc: "/null",
         name: "N/A",
         bio: "N/A",
       };
-    });
-
-    test("only notice that preview is unavailable is displayed with shortform on, available off", () => {
-      profile.available = false;
-      profile.shortForm = true;
-
       Object.freeze(profile);
 
       const container = ProfilePreview(profile, {
@@ -71,6 +52,11 @@ describe("profile preview", () => {
     });
 
     test("only notice that preview is unavailable is displayed with shortform off, available off", () => {
+      const profile = {
+        pictureSrc: "/null",
+        name: "N/A",
+        bio: "N/A",
+      };
       Object.freeze(profile);
 
       const container = ProfilePreview(profile, {
@@ -86,30 +72,23 @@ describe("profile preview", () => {
   });
 
   describe.skip("short form option", () => {
-    const profile = {
-      pictureSrc: "/picture",
-      name: "Jane Doe",
-      bio: "Duis porta neque sed eros.",
-    };
-    Object.freeze(profile);
+    test("contains profile info except the bio with shortform on", () => {
+      const profile = {
+        pictureSrc: "/picture",
+        name: "Jane Doe",
+        bio: "Duis porta neque sed eros.",
+      };
+      Object.freeze(profile);
 
-    const container = ProfilePreview(profile, {
-      shortForm: true,
-    });
+      const container = ProfilePreview(profile, {
+        shortForm: true,
+      });
 
-    test("contains provided image", () => {
       const image = getByTestId(container, "profilePicture");
-
-      expect(image.src).toContain(profile.pictureSrc);
-    });
-
-    test("contains provided name", () => {
       const name = getByTestId(container, "profileName");
 
+      expect(image.src).toContain(profile.pictureSrc);
       expect(name).toHaveTextContent(profile.name);
-    });
-
-    test("contains 2 elements", () => {
       expect(container.childElementCount).toBe(2);
     });
   });
