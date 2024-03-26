784b8edf8a50653ee46ddf28a2fb0e57ed4a1c75
girdenis-p <girdenis.p@gmail.com>
2024-03-26 16:45:41 +0000

    update buttonAndCounter identifier to main

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -14,12 +14,12 @@ export function App() {
     `;
   body.appendChild(header);
 
-  const buttonAndCounter = document.createElement("main");
-  buttonAndCounter.innerHTML = `
+  const main = document.createElement("main");
+  main.innerHTML = `
         <p id="counter" data-testid="counter">0</p>
         <button id="increment">Increment</button>
     `;
-  body.appendChild(buttonAndCounter);
+  body.appendChild(main);
 
   const button = body.querySelector("#increment");
   const counter = body.querySelector("#counter");
