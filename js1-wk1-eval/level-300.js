69379eb0f8013b9b24aba66445d7a24c5a471958
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-10-26 14:05:35 +0100

    format date and time display using template literals with slices for day and month

--- js1-wk1-eval/level-300.js
+++ js1-wk1-eval/level-300.js
@@ -8,5 +8,12 @@ const time = "21:49";
 const macTimeDisplay = `${day.slice(0,3)} ${date} ${month.slice(0,3)} ${time}`;
 
 // Before running code, answer the following - write down your answers:
+// Fri 29 Sep 21:49
+
 // a) how many function calls are there in this file?
+// Two
+
 // b) predict and explain what macTimeDisplay will evaluate to. You can use documentation to look up slice
+console.log(macTimeDisplay);
+// macTimeDisplay will evaluate to "Fri 29 Sep 21:49" by slicing "Friday" and "September" to "Fri" and "Sep", then combining with date and time.
+
