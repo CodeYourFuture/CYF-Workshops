154f072c3df223aa6df58f1c69cac4c51bdd911c
Benson Arafat <benpaul320@gmail.com>
2024-11-30 12:41:57 +0100

    add-decrement-button

---
+++ dom-merge-conflict/tasks/buttons-and-counter/src/header.js
@@ -0,0 +1,25 @@
+//increments the number in a node's text
+function increment(node) {
+    let current = node.textContent;
+    node.textContent = Number(current) + 1;
+  }
+  
+  export function App() {
+    const body = document.createElement("body");
+  
+    const header = document.createElement("header");
+    header.innerHTML = `
+          <h1>Number Counter</h1>
+          <p>A simple counter. Press increment to increase the count by one.</p>
+      `;
+    body.appendChild(header);
+  
+    const button = body.querySelector("#increment");
+    const counter = body.querySelector("#counter");
+    button.addEventListener("click", () => {
+      increment(counter);
+    });
+  
+    return body;
+  }
+  
