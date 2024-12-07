1f095fe226e38a523b1006ab6754cd8272dd6519
Emmanuel <esiziba71@gmail.com>
2024-12-07 12:13:06 +0200

    update app.js

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -1,31 +1,73 @@
 //increments the number in a node's text
+
 function increment(node) {
+
   let current = node.textContent;
+
   node.textContent = Number(current) + 1;
+
+}
+
+//decrements the number in a node's text
+
+function decrement(node) {
+
+  let current = node.textContent;
+
+  node.textContent = Number(current) - 1;
+
 }
 
 export function App() {
+
   const body = document.createElement("body");
 
   const header = document.createElement("header");
+
   header.innerHTML = `
+
         <h1>Number Counter</h1>
+
         <p>A simple counter. Press increment to increase the count by one.</p>
+
+        <p>A simple counter. Press decrement to decrease the count by one.</p>
+
     `;
+
   body.appendChild(header);
 
   const main = document.createElement("main");
+
   main.innerHTML = `
+
         <p id="counter" data-testid="counter">0</p>
+
         <button id="increment">Increment</button>
+
+        <button id="decrement">Decrement</button>
+
     `;
+
   body.appendChild(main);
 
   const button = body.querySelector("#increment");
+
+  const decrementButton = body.querySelector("#decrement");
+
   const counter = body.querySelector("#counter");
+
   button.addEventListener("click", () => {
+
     increment(counter);
+
+  });
+
+  decrementButton.addEventListener("click", () => {
+
+    decrement(counter);
+
   });
 
   return body;
+
 }
