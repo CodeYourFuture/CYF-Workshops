75a182d51bf85bec3525c3b4c3aca81451db7462
Sally McGrath <sally@codeyourfuture.io>
2024-12-16 16:42:10 +0000

    netlify maybe triggering a reload?

--- asking-questions/script.js
+++ asking-questions/script.js
@@ -1,3 +1,10 @@
-if (location.hash === '#problem-bank') {
-    document.querySelector('details#problem-bank')?.open = true;
+//netlify triggers a reload?
+
+function openDetailsIfHashPresent() {
+    if (location.hash === '#problem-bank') {
+        document.querySelector('details#problem-bank')?.open = true;
+    }
 }
+
+openDetailsIfHashPresent();
+window.addEventListener('load', openDetailsIfHashPresent);
