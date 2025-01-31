43e1c1e52b0fe2edf67f061a838da04a569ea307
Paul McKenna <11870096+paulmck21@users.noreply.github.com>
2025-01-31 20:50:19 +0000

    Change avgDaysInYear to avgDaysInMonth

--- js1-wk1-eval/level-500.js
+++ js1-wk1-eval/level-500.js
@@ -1,7 +1,7 @@
 
 
-const avgDaysInYear = Math.floor(365 / 12);
+const avgDaysInMonth = Math.floor(365 / 12);
 
 // Without running any code,
-// Predict and explain what the value of avgDaysInYear will evaluate to
+// Predict and explain what the value of avgDaysInMonth will evaluate to
 // You can use documentation to look up what Math.floor does
