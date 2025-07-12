9bf69db6930f225c5ef6797fe8569875f62606cf
obie <obertilimandzi@gmail.com>
2025-07-12 13:52:53 +0200

    split app

---
+++ dom-merge-conflict/tasks/buttons-and-counter/src/header.js
@@ -0,0 +1,7 @@
+  export function header() {
+  
+  const header = document.createElement("header");
+  header.innerHTML = `
+        <h1>Number Counter</h1>
+        <p>A simple counter. Press increment to increase the count by one.</p>
+    `;}
