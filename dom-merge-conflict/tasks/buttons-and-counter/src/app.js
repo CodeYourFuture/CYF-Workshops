13fe2926af74a0521ad999da1c6cded880a44d9b
Nahom Mesfin <mnahom545@gmail.com>
2025-07-19 11:30:44 +0100

    split header and main

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -1,4 +1,6 @@
 //increments the number in a node's text
+import { header } from "./header.js";
+import { main } from "./main.js";
 function increment(node) {
   let current = node.textContent;
   node.textContent = Number(current) + 1;
@@ -7,18 +9,10 @@ function increment(node) {
 export function App() {
   const body = document.createElement("body");
 
-  const header = document.createElement("header");
-  header.innerHTML = `
-        <h1>Number Counter</h1>
-        <p>A simple counter. Press increment to increase the count by one.</p>
-    `;
+
   body.appendChild(header);
 
-  const main = document.createElement("main");
-  main.innerHTML = `
-        <p id="counter" data-testid="counter">0</p>
-        <button id="increment">Increment</button>
-    `;
+
   body.appendChild(main);
 
   const button = body.querySelector("#increment");
---
+++ dom-merge-conflict/tasks/buttons-and-counter/src/main.js
@@ -0,0 +1,5 @@
+  export const main = document.createElement("main");
+  main.innerHTML = `
+        <p id="counter" data-testid="counter">0</p>
+        <button id="increment">Increment</button>
+    `;
