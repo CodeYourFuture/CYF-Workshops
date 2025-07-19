32e9741f69687d5924e4b9703f774c7a204b1ae6
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-07-19 11:26:33 +0100

    add decrement button

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -3,6 +3,10 @@ function increment(node) {
   let current = node.textContent;
   node.textContent = Number(current) + 1;
 }
+function decrement(node) {
+  let current = node.textContent;
+  node.textContent = Number(current) - 1;
+}
 
 export function App() {
   const body = document.createElement("body");
@@ -22,13 +26,14 @@ export function App() {
   body.appendChild(main);
 
   const button = body.querySelector("#increment");
+  const buttonDecrement = body.querySelector("#decrement");
   const counter = body.querySelector("#counter");
   button.addEventListener("click", () => {
     increment(counter);
-  }); 
-
-
+  });
+  buttonDecrement.addEventListener("click", () => {
+    decrement(counter);
+  });
 
   //world
- 
 }
