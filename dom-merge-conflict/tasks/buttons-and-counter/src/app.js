b2ffb65a58adc48f49f50f05f0a50dfa4f30f797
avatarit <salah@ait.ps>
2025-07-19 11:39:21 +0100

    refactor: modularize app structure by creating header and main components

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -1,34 +1,14 @@
-//increments the number in a node's text
-function increment(node) {
-  let current = node.textContent;
-  node.textContent = Number(current) + 1;
-}
+import { createHeader } from "./header.js";
+import { createMain } from "./main.js";
 
 export function App() {
   const body = document.createElement("body");
 
-  const header = document.createElement("header");
-  header.innerHTML = `
-        <h1>Number Counter</h1>
-        <p>A simple counter. Press increment to increase the count by one.</p>
-    `;
-  body.appendChild(header);
+  const header = createHeader();
+  const main = createMain();
 
-  const main = document.createElement("main");
-  main.innerHTML = `
-        <p id="counter" data-testid="counter">0</p>
-        <button id="increment">Increment</button>
-    `;
+  body.appendChild(header);
   body.appendChild(main);
 
-  const button = body.querySelector("#increment");
-  const counter = body.querySelector("#counter");
-  button.addEventListener("click", () => {
-    increment(counter);
-  }); 
-
-
-
-  //world
- 
+  return body;
 }
