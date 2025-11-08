7382844b1142b1fa096ae4d02eba39c822299faf
UbunMen <mahtem23@gmail.com>
2025-11-08 12:12:57 +0000

    Buttons and counter tested

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -4,13 +4,18 @@ function increment(node) {
   node.textContent = Number(current) + 1;
 }
 
+function decrement(node) {
+  let current = node.textContent;
+  node.textContent = Number(current) - 1;
+}
+
 export function App() {
   const body = document.createElement("body");
 
   const header = document.createElement("header");
   header.innerHTML = `
         <h1>Number Counter</h1>
-        <p>A simple counter. Press increment to increase the count by one.</p>
+        <p>A simple counter. Press increment or decrement to increase the count by one.</p>
     `;
   body.appendChild(header);
 
@@ -18,14 +23,21 @@ export function App() {
   main.innerHTML = `
         <p id="counter" data-testid="counter">0</p>
         <button id="increment">Increment</button>
+        <button id="decrement">Decrement</button>
     `;
   body.appendChild(main);
 
-  const button = body.querySelector("#increment");
+  const icncrementButton = body.querySelector("#increment");
   const counter = body.querySelector("#counter");
-  button.addEventListener("click", () => {
+  const decrementButton = body.querySelector("#decrement");
+
+  icncrementButton.addEventListener("click", () => {
     increment(counter);
   });
-
+  decrementButton.addEventListener("click", () => {
+    decrement(counter);  
+  });
+  
   return body;
 }
+
--- dom-merge-conflict/tasks/buttons-and-counter/test/app.test.js
+++ dom-merge-conflict/tasks/buttons-and-counter/test/app.test.js
@@ -35,7 +35,7 @@ describe("button and counter", () => {
     expect(getByTestId(container, "counter")).toHaveTextContent(/^2$/);
   });
 
-  describe.skip("decrement button", () => {
+  describe("decrement button", () => {
     test("pressing Decrement decreases the counter", () => {
       const button = getByRole(container, "button", {
         name: "Decrement",
