adc0faa1cca0fdcdf2415a493dac461ad39d4270
HANISADAH <hanisadah81@gmail.com>
2025-11-08 11:43:10 +0000

    seperated header and main

---
+++ dom-merge-conflict/tasks/buttons-and-counter/src/header.js
@@ -0,0 +1,11 @@
+    
+export default function header() {
+    const header = document.createElement("header");
+  header.innerHTML = `
+        <h1>Number Counter</h1>
+        <p>A simple counter. Press increment to increase the count by one.</p>
+    `;
+    return header;
+     
+    
+ }
---
+++ dom-merge-conflict/tasks/buttons-and-counter/src/main.js
@@ -0,0 +1,8 @@
+export default function main() {
+    const main = document.createElement("main");
+  main.innerHTML = `
+        <p id="counter" data-testid="counter">0</p>
+        <button id="increment">Increment</button>
+    `;
+    return main;
+ }
