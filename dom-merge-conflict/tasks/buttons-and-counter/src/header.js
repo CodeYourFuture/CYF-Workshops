5ce564620ac09708956ec45e5ca4ec62ff35acfb
Ali <naru.a@yahoo.com>
2025-11-08 11:14:04 +0000

    Add empty header and main component files

---
+++ dom-merge-conflict/tasks/buttons-and-counter/src/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Welcome to My App</h1>
+    </header>
+  );
+}
---
+++ dom-merge-conflict/tasks/buttons-and-counter/src/main.js
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main>
+      <p>This is the main content of the app.</p>
+    </main>
+  );
+}
