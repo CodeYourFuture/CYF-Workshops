ee70b8467dc5285de1ea7747b18aef40a18f37ae
Steven van Blerk <stevenvanblerkrsa@gmail.com>
2025-11-26 20:46:58 +0200

    comment tweak

--- introduction-to-typescript/src/exercise3.ts
+++ introduction-to-typescript/src/exercise3.ts
@@ -1,6 +1,7 @@
 /** Your goal for this exercise is to reasonably handle the type of an error appearing within a try catch block.
  * You'll notice that error.message is reporting a typescript error. There are a number of ways that you could address this typescript error.
- * Explore your options and be sure to have reasoning for the solution you've chosen. When ready, ask a volunteer for feedback on your solution.
+ * You are welcome to research online to explore your options. Be sure to have reasoning for the solution you've chosen.
+ * When ready, ask a volunteer for feedback on your solution.
  *
  * There is a test file for this exercise but it will only confirm that the function still works at runtime.
  * npm run test-exercise4
