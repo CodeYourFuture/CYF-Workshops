6d006af799782b748eb405c7f29a1e32b4382ec0
Steven van Blerk <stevenvanblerkrsa@gmail.com>
2025-11-26 20:15:54 +0200

    tiny typo fix

--- introduction-to-typescript/src/tests/exercise3.test.ts
+++ introduction-to-typescript/src/tests/exercise3.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { tryCatchDemo } from "../exercise3";
 
-/** Do not change the tests below. All tests can be made to pass by editing the exercise1.ts file instead */
+/** Do not change the tests below. All tests can be made to pass by editing the exercise3.ts file instead */
 describe("tryCatchDemo", () => {
   it("does not return an error message when state is 'succeed'", () => {
     expect(tryCatchDemo("succeed")).toEqual("No errors!");
