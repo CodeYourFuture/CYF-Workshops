dc00a2c9c38dcf0cec70797626df45fedf23b6b6
Justice <cyf@cyfs-MacBook-Pro.local>
2026-03-14 11:52:37 +0000

    added new components header.js and main.js and updated code

---
+++ dom-merge-conflict/tasks/buttons-and-counter/src/main.js
@@ -0,0 +1,8 @@
+function main(body) {
+   const main = document.createElement("main");
+  main.innerHTML = `
+        <p id="counter" data-testid="counter">0</p>
+        <button id="increment">Increment</button>
+    `;
+  body.appendChild(main); 
+}
