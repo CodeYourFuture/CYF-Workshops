4ca50c67f808ed2022ad474f535a8a85403d1c46
CJ Yuan <cjyuan00@gmail.com>
2026-03-28 11:29:25 +0000

    Update app.js

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -13,7 +13,14 @@ export function App() {
         <p>A simple counter. Press increment to increase the count by one.</p>
     `;
   body.appendChild(header);
+  const body = document.createElement("body");
 
+  const header = document.createElement("header");
+  header.innerHTML = `
+        <h1>Number Counter</h1>
+        <p>A simple counter. Press increment to increase the count by one.</p>
+    `;
+  body.appendChild(header);
   const main = document.createElement("main");
   main.innerHTML = `
         <p id="counter" data-testid="counter">0</p>
@@ -26,6 +33,21 @@ export function App() {
   button.addEventListener("click", () => {
     increment(counter);
   });
+  const body = document.createElement("body");
 
-  return body;
+  const header = document.createElement("header");
+  header.innerHTML = `
+        <h1>Number Counter</h1>
+        <p>A simple counter. Press increment to increase the count by one.</p>
+    `;
+  body.appendChild(header);
+  return undefined;
+    const body = document.createElement("body");
+
+  const header = document.createElement("header");
+  header.innerHTML = `
+        <h1>Number Counter</h1>
+        <p>A simple counter. Press increment to increase the count by one.</p>
+    `;
+  body.appendChild(header);
 }
