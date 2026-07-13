68d790ef018fec96339bdd24907983090d28e0d4
Liam Greenfield <52577696+Liam310@users.noreply.github.com>
2026-07-13 12:39:29 +0100

    remove whitespace

--- arrays/300.js
+++ arrays/300.js
@@ -1,7 +1,6 @@
 // the head of an array is the first element of an array
 // the tail of an array is the last element of an array
 
-
 const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 function getHead(arr) {
@@ -15,5 +14,6 @@ function getTail(arr) {
 console.log(`The first letter of the alphabet is ${getHead(alphabet)}`);
 console.log(`The last letter of the alphabet is ${getTail(alphabet)}`);
 console.log(alphabet.length);
+
 // Explain why alphabet length is now 24
 // How could we change the functions we defined to return the same values, but not change the length of alphabet?
